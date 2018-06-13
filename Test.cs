91f280ca7af4bf930b76d7a413b4f1666e8f03bc
yklstam <841741474@qq.com>
2018-06-13 11:03:51 +0800

    My First C#

---
+++ Test.cs
@@ -0,0 +1,15 @@
+/*
+我的第一个C#程序
+*/
+using System;
+namespace MySharp
+{
+class MyClass
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("我爱C#!");
+        Console.ReadLine();
+    }
+}
+}
