44ccff7101b5cf17a64d3bd811a4a42c897e57a4
yklstam <841741474@qq.com>
2018-06-13 13:42:56 +0800

    My First C#

--- Test.cs
+++ Test.cs
@@ -1,15 +1,15 @@
 /*
 我的第一个C#程序
 */
-using System;
-namespace MySharp
-{
-class MyClass
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("我爱C#!");
-        Console.ReadLine();
-    }
-}
-}
+using System;    //导入System命名空间
+namespace MySharp  //自定义Mysharp命名空间
+{                  //命名空间的开始大括弧
+class MyClass      //自定义MyClass类
+{                   //类开始的大括弧
+    static void Main(string[] args)   //入口方法Main
+    {                                   //Main方法的开始大括弧
+        Console.WriteLine("我爱C#!");   //向屏幕输出信息语句
+        Console.ReadLine();            //从键盘读取信息语句
+    }                                 //Main方法的结束大括弧   
+}                                     //MyClass类的结束大括弧
+}                                     //命名空间的结束大括弧
