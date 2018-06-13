399f0fa3f94187bffa9d4e663606f6c99357eac7
yklstam <841741474@qq.com>
2018-06-13 15:34:38 +0800

    变量学习

---
+++ salary.cs
@@ -0,0 +1,16 @@
+using System;
+namespace salary
+{
+    class salary
+    {
+    [STATHread]
+    static void Main(string[] args){
+float salary = 1000.0f;
+float rate = 0.05f;
+double money;
+money = salary - salary * rate;
+console.writeline("你的收入是：{0,2:F}",money);
+console.readline();
+    }    
+    }
+}
