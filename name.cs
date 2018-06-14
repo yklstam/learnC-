1df0dc23b74a6bf8532117f786a1c5cae930a6d0
yklstam <841741474@qq.com>
2018-06-14 09:30:17 +0800

    变量练习

---
+++ name.cs
@@ -0,0 +1,15 @@
+using System;
+namespace name
+{
+class name
+{
+    static void Main(string[] args){
+string hello="你好";
+Console.WriteLine("请输入姓名：");
+string name= Console.ReadLine();
+string r=hello+name+"!";
+Console.WriteLine(r);
+Console.ReadLine();
+    }
+}   
+}
